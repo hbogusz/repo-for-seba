cc8c46ce960d84d2734677079258f1f2865d4f35
First Last <you@email.com>
2022-07-04 13:12:04 +0200

    code cleaning

--- force-app/main/default/lwc/searchResults/searchResults.js
+++ force-app/main/default/lwc/searchResults/searchResults.js
@@ -10,24 +10,17 @@ export default class SearchResults extends LightningElement {
     noResults=false;
     resultsSize=0;
 
-    connectedCallback() {
-        console.log('initial');
-        console.log('term:', this.term);
-    }
-
     @wire(searchProducts, {searchTerm: '$term'})
     wiredProducts(result) {
         this.wiredSearchResult = result;
         if (result.data) {
             this.searchedProducts = result.data;
             this.resultsSize = this.searchedProducts.length;
-            console.log(this.resultsSize);
             if(this.resultsSize == 0){
                 this.noResults = true;
             } else {
                 this.noResults = false;
             }
-            console.log('success', result.data);
         } else if (result.error) {
             console.log('data.error');
             console.log(result.error);
