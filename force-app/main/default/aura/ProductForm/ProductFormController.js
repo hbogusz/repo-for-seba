67a4ec1df0b5a72bed74d05bad87a8de4962e0b5
First Last <you@email.com>
2022-07-12 15:13:03 +0200

    product adding fixed

--- force-app/main/default/aura/ProductForm/ProductFormController.js
+++ force-app/main/default/aura/ProductForm/ProductFormController.js
@@ -26,25 +26,36 @@
     },
     manageImg: function (component, event, helper) {
         var modalBody;
-        var recordId = component.get('v.recordId');
-        if(component.get('v.price') != '' && component.get('v.name') != '' && component.get('v.price') != undefined && component.get('v.name') != undefined){
+        var price = component.find('priceInput');
+
+        if (component.get('v.price') != '' && component.get('v.price') != undefined && component.get('v.name') != '' && component.get('v.name') != undefined) {
             $A.createComponent("c:ImgManager", {},
-            function (content, status) {
-                if (status === "SUCCESS") {
-                    modalBody = content;
-                    var modalPromise = component.find('overlayImg').showCustomModal({
-                        header: "Image Manager",
-                        body: modalBody,
-                        showCloseButton: true
-                    });
-                    component.set("v.modalPromise", modalPromise);
-                }
+                function (content, status) {
+                    if (status === "SUCCESS") {
+                        modalBody = content;
+                        var modalPromise = component.find('overlayImg').showCustomModal({
+                            header: "Image Manager",
+                            body: modalBody,
+                            showCloseButton: true
+                        });
+                        component.set("v.modalPromise", modalPromise);
+                    }
+                });
+        } else {
+            $A.util.addClass(price, "slds-has-error");
+            component.set("v.priceError", true);
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": "Error!",
+                "message": 'Fields: Name and Price must be filled out!',
+                "type": "error"
             });
+            toastEvent.fire();
         }
     },
     closeModal: function (component, event, helper) {
         var mainImg = event.getParam('imgId');
-        if(mainImg == null){
+        if (mainImg == null) {
             component.set('v.imgId', "/resource/1656320785000/addImage");
         } else {
             component.get('v.modalPromise').then(function (modal) {
@@ -56,22 +67,15 @@
     },
     handleSaving: function (component, event, helper) {
         var recordId = component.get('v.recordId');
-        if (recordId != undefined) {
-            if (component.get('v.price') == undefined) {
-                var noPriceEvent = $A.get("e.force:showToast");
-                noPriceEvent.setParams({
-                    "title": "Error!",
-                    "message": 'You should add price to be able to save product',
-                    "type": "error"
-                });
-                noPriceEvent.fire();
-            } else {
+        var price = component.find('priceInput');
+        if (component.get('v.price') != '' && component.get('v.price') != undefined && component.get('v.name') != '' && component.get('v.name') != undefined) {
+            if (recordId != undefined) {
                 component.set("v.spinner", true);
                 helper.addPricebookEntry(component);
                 var navigation = component.find('navigation');
                 setTimeout(() => {
-                component.set("v.spinner", false);
-                navigation.navigate({
+                    component.set("v.spinner", false);
+                    navigation.navigate({
                         'type': 'standard__recordPage',
                         'attributes': {
                             'objectApiName': 'Product2',
@@ -80,24 +84,64 @@
                         }
                     });
                 }, "1000")
-
+            } else {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Error!",
+                    "message": 'You should add some image to be able to save product',
+                    "type": "error"
+                });
+                toastEvent.fire();
             }
         } else {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
+            $A.util.addClass(price, "slds-has-error");
+            component.set("v.priceError", true);
+            var errorToast = $A.get("e.force:showToast");
+            errorToast.setParams({
                 "title": "Error!",
-                "message": 'You should add some image to be able to save product',
+                "message": 'Fields: Name and Price must be filled out!',
                 "type": "error"
             });
-            toastEvent.fire();
+            errorToast.fire();
         }
 
     },
     handleSavingNew: function (component, event, helper) {
-        helper.addPricebookEntry(component);
-        setTimeout(() => {
-            $A.get('e.force:refreshView').fire();
-        }, "1000")
+        var price = component.find('priceInput');
+        var recordId = component.get('v.recordId');
+        if (component.get('v.price') != '' && component.get('v.price') != undefined && component.get('v.name') != '' && component.get('v.name') != undefined) {
+            if (recordId != undefined) {
+                component.set("v.spinner", true);
+                helper.addPricebookEntry(component);
+                setTimeout(() => {
+                    $A.get('e.force:refreshView').fire();
+                }, "1000")
+            } else {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Error!",
+                    "message": 'You should add some image to be able to save product',
+                    "type": "error"
+                });
+                toastEvent.fire();
+            }
+        } else {
+            $A.util.addClass(price, "slds-has-error");
+            component.set("v.priceError", true);
+            var errorToast = $A.get("e.force:showToast");
+            errorToast.setParams({
+                "title": "Error!",
+                "message": 'Fields: Name and Price must be filled out!',
+                "type": "error"
+            });
+            errorToast.fire();
+        }
+    },
+
+    onPriceChange: function (component, event, helper) {
+        var price = component.find('priceInput');
+        $A.util.removeClass(price, "slds-has-error");
+        component.set("v.priceError", false);
     },
 
 
