51ea06f52a5e9b25bee1e52276f75da39fed7b33
First Last <you@email.com>
2022-07-18 14:37:02 +0200

    initial

--- force-app/main/default/lwc/cartPage/cartPage.js
+++ force-app/main/default/lwc/cartPage/cartPage.js
@@ -3,6 +3,7 @@ import { LightningElement, wire } from 'lwc';
 import getItemsFromCart from '@salesforce/apex/CartController.getItemsFromCart';
 import emptyCart from '@salesforce/apex/CartController.clearCart';
 import createOrder from '@salesforce/apex/CartController.createOrder';
+import checkDiscountCode from '@salesforce/apex/CartController.checkDiscountCode';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -14,6 +15,10 @@ export default class CartPage extends LightningElement {
     noResults;
     isLoading = false;
     totalPrice = 0;
+    totalPriceWithDiscount = 0;
+    discountCode;
+    noDiscount = true;
+    discount = 0;
 
     @wire(getItemsFromCart)
     wiredProducts(result) {
@@ -29,6 +34,14 @@ export default class CartPage extends LightningElement {
             this.items.forEach(item => {
                 this.totalPrice += item.price * item.quantity;
             });
+            if (this.discount > 0) {
+                this.items.forEach(item => {
+                    this.totalPriceWithDiscount += (100 - this.discount) / 100 * item.price * item.quantity;
+                });
+            }
+
+
+
         } else if (result.error) {
             console.log('data.error');
             console.log(result.error);
@@ -49,38 +62,75 @@ export default class CartPage extends LightningElement {
     refreshCart() {
         refreshApex(this.wiredItemsResult);
     }
-    makeOrder(){
+    makeOrder() {
         this.isLoading = true;
         createOrder()
-        .then(() => {
-            this.dispatchEvent(ShowToastEvent({
-                title: 'Success!',
-                message: 'Order placed succesfully!',
-                variant: 'success'
-            })
-            );
-            emptyCart()
             .then(() => {
-            this.isLoading = false;
-            location.href =  '/Community/s/orders';
+                this.dispatchEvent(ShowToastEvent({
+                    title: 'Success!',
+                    message: 'Order placed succesfully!',
+                    variant: 'success'
+                })
+                );
+                emptyCart()
+                    .then(() => {
+                        this.isLoading = false;
+                        location.href = '/Community/s/orders';
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        this.isLoading = false;
+                    });
+
             })
             .catch((error) => {
                 console.error(error);
+                const event = new ShowToastEvent({
+                    title: 'Error!',
+                    message: 'Unexpected error has occured: ' + error,
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
                 this.isLoading = false;
             });
-            
-        })
-    .catch((error) => {
-        console.error(error);
-        const event = new ShowToastEvent({
-            title: 'Error!',
-            message: 'Unexpected error has occured: ' + error,
-            variant: 'error'
-        });
-        this.dispatchEvent(event);
-        this.isLoading = false;
-    });
     }
-    
-
+    setDiscount(event) {
+        this.discountCode = event.target.value;
+    }
+    useDiscountCode() {
+        this.isLoading = true;
+        checkDiscountCode({ discountCode: this.discountCode, })
+            .then((result) => {
+                if (result > 0) {
+                    this.dispatchEvent(ShowToastEvent({
+                        title: 'Success!',
+                        message: 'Discount code is correct!',
+                        variant: 'success'
+                    })
+                    );
+                    this.noDiscount = false;
+                    this.discount = result;
+                    refreshApex(this.wiredItemsResult);
+                } else {
+                    const event = new ShowToastEvent({
+                        title: 'Error!',
+                        message: 'Provided discount code is incorrect. Check spelling',
+                        variant: 'error'
+                    });
+                    this.dispatchEvent(event);
+                    this.noDiscount = true;
+                }
+                this.isLoading = false;
+            })
+            .catch((error) => {
+                console.error(error);
+                const event = new ShowToastEvent({
+                    title: 'Error!',
+                    message: 'Unexpected error: ' + error,
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
+                this.isLoading = false;
+            });
+    }
 }
