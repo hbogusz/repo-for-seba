5afe5ab340b5bb3ed6daba76b05a764831575662
hbogusz <107925387+hbogusz@users.noreply.github.com>
2022-07-26 14:35:49 +0200

    Revert "Revert "Revert "Revert "Revert "Develop"""""

--- force-app/main/default/aura/PricebookManager/PricebookManagerController.js
+++
@@ -1,8 +0,0 @@
-({
-    spinnerOn: function(component, event, helper) {
-        component.set("v.spinner", true);
-    },
-    spinnerOff: function(component, event, helper) {
-        component.set("v.spinner", false);
-    }
-})
